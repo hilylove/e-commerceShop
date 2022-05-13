4744e3426a54ab59e6f6385e91108478d762031b
Ashley <ashleyshi006@gmail.com>
2022-05-12 20:48:48 -0400

    updating cartContext to use reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useReducer } from "react";
 
+import { createAction } from "../utils/reducer/reducer.utiles";
+
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -40,7 +42,7 @@ export const UserProvider = ({ children }) => {
   console.log(currentUser);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
