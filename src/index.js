3f0feb34372ff4620a0cc50490e114bb0b38bf42
Ashley <ashleyshi006@gmail.com>
2022-05-14 15:20:39 -0400

    moving user and categories context into  redux

--- src/index.js
+++ src/index.js
@@ -1,43 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
-import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
+import { store } from "./store/store";
 
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
-
-// ReactDOM.render(
-// <React.StrictMode>
-//   <BrowserRouter>
-//     <UserProvider>
-//       <CategoriesProvider>
-//         <CartProvider>
-//           <App />
-//         </CartProvider>
-//       </CategoriesProvider>
-//     </UserProvider>
-//   </BrowserRouter>
-// </React.StrictMode>,
-//   document.getElementById("root")
-// );
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
